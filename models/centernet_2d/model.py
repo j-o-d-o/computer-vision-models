4a9ed11982388516165b1e870cda2b203fa81a05
j-o-d-o <jdobler@protonmail.com>
2020-11-29 14:03:18 +0100

    mobilev2 base net

--- models/centernet_2d/model.py
+++ models/centernet_2d/model.py
@@ -1,98 +1,142 @@
+# Check: https://coral.ai/docs/edgetpu/models-intro/#supported-operations for supported ops on EdgeTpu
 import tensorflow as tf
+from tensorflow.keras import initializers, regularizers, Model
+from tensorflow.keras.layers import Conv2DTranspose, BatchNormalization, ReLU, Conv2D, DepthwiseConv2D, Add, Input, concatenate
+from tensorflow.raw_ops import ResizeNearestNeighbor
 import numpy as np
-import tensorflow.python.keras.applications.efficientnet as efficientnet
-from models.centernet_2d import Params
 
 
-def deConv2DBatchNorm(inputs, filters=128, kernel_size=(3, 3), strides=(2, 2), padding="same", name=None):
-    """
-    Deconvolution with BatchNormalization and activation
-    :param inputs: input to the layer
-    :param filters: number of filters from convoltuion, defaults to 128
-    :param kernel_size: kernel for convolution, defaults to (2, 2)
-    :param strides: strides of convolution, defaults to (2, 2)
-    :param name: name of the scope
-    :return: output deconvolution layer
-    """
-    layer = tf.keras.layers.Conv2DTranspose(
-        filters,
-        kernel_size,
-        strides=strides,
-        padding=padding,
-        use_bias=False,
-        kernel_initializer="he_uniform",
-        kernel_regularizer=tf.keras.regularizers.l2(1.25e-5),
-        name=name + "_deconv2D"
-    )(inputs)
-    layer = tf.keras.layers.BatchNormalization(name=name + "_batnchnorm")(layer)
-    layer = tf.keras.layers.Activation("relu", name=name + "_activation")(layer)
+def deconv_2d(inputs, filters=128, kernel_size=(3, 3), strides=(2, 2), padding="same"):
+    layer = Conv2DTranspose(filters, kernel_size, strides=strides, padding=padding, use_bias=False,
+        kernel_initializer="he_uniform", kernel_regularizer=regularizers.l2(1.25e-5))(inputs)
+    layer = BatchNormalization()(layer)
+    layer = ReLU()(layer)
     return layer
 
+def _bottle_neck_block(inputs, filters: int, expansion_factor: int = 6, stride=1, bn_alpha=1):
+  filters = max(2, int(filters * bn_alpha))
+  x = inputs
+
+  # Expansion
+  x = Conv2D(filters * expansion_factor, kernel_size=1, padding='same', use_bias=False, activation=None)(x)
+  x = BatchNormalization()(x)
+  x = ReLU(6.)(x)
+
+  # Convolution
+  x = DepthwiseConv2D(kernel_size=3, strides=stride, activation=None, use_bias=False,
+                      padding='same' if stride == 1 else 'same')(x)
+  x = BatchNormalization()(x)
+  x = ReLU(6.)(x)
+
+  # Project
+  x = Conv2D(filters, kernel_size=1, padding='same', use_bias=False, activation=None)(x)
+  x = BatchNormalization()(x)
+
+  # Residual connection
+  input_filters = int(inputs.shape[-1])
+  if stride == 1 and input_filters == filters:
+    x = Add()([inputs, x])
+
+  return x
+
 def create_model(input_height, input_width, mask_height, mask_width, nb_classes):
-    """
-    Using some pretrained classifier model on imagenet from the keras application zoo as base,
-    add the needed layers to get to the output mask size
-    """
-    shape = (input_height, input_width, 3)
-    base_model = efficientnet.EfficientNetB2(input_shape=shape, include_top=False, weights='imagenet')
+    bn_alpha = 1
+    inputs = Input(shape=(input_height, input_width, 3))
+    x = Conv2D(max(2, int(32 * bn_alpha)), 5, padding="same", strides=(2, 2))(inputs) # / 2
+    x = BatchNormalization()(x)
+    x = ReLU(6.)(x)
+
+    features = []
+    features.append(x)
+
+    x = _bottle_neck_block(x, 32, expansion_factor=1, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 32, expansion_factor=6, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 32, expansion_factor=6, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 32, expansion_factor=6, stride=2, bn_alpha=bn_alpha) # / 2
+    features.append(x)
+
+    x = _bottle_neck_block(x, 64, expansion_factor=6, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 64, expansion_factor=6, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 64, expansion_factor=6, stride=2, bn_alpha=bn_alpha) # / 2
+    features.append(x)
+
+    x = _bottle_neck_block(x, 98, expansion_factor=6, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 98, expansion_factor=6, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 98, expansion_factor=6, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 98, expansion_factor=6, stride=2, bn_alpha=bn_alpha) # / 2
+    features.append(x)
+
+    x = _bottle_neck_block(x, 128, expansion_factor=6, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 128, expansion_factor=6, stride=1, bn_alpha=bn_alpha)
+    x = _bottle_neck_block(x, 128, expansion_factor=6, stride=2, bn_alpha=bn_alpha) # / 2
+    features.append(x)
+
+    # features sizes with input (92, 308)
+    # [0]: (None, 46, 154, 32)
+    # [1]: (None, 23, 77, 32)
+    # [2]: (None, 12, 39, 64)
+    # [3]: (None, 6, 20, 98)
+    # [4]: (None, 3, 10, 128)
 
     # Extract feature layers and upsample them to the output shape
     output_shape = (mask_height, mask_width)
-    # block2c_activation
-    feat_0 = deConv2DBatchNorm(base_model.get_layer("block2c_activation").output, filters=16, name="up0_feat_0")
-    feat_0 = tf.raw_ops.ResizeNearestNeighbor(images=feat_0, size=output_shape, name="feat_0_resize")
-    # block3c_activation
-    feat_1 = deConv2DBatchNorm(base_model.get_layer("block3c_activation").output, filters=16, name="up0_feat_1")
-    feat_1 = deConv2DBatchNorm(feat_1, filters=8, name="up1_feat_1")
-    feat_1 = tf.raw_ops.ResizeNearestNeighbor(images=feat_1, size=output_shape, name="feat_1_resize")
-    # block5d_activation
-    feat_2 = deConv2DBatchNorm(base_model.get_layer("block5d_activation").output, filters=32, name="up0_feat_2")
-    feat_2 = deConv2DBatchNorm(feat_2, filters=16, name="up1_feat_2")
-    feat_2 = deConv2DBatchNorm(feat_2, filters=8, name="up2_feat_2")
-    feat_2 = tf.raw_ops.ResizeNearestNeighbor(images=feat_2, size=output_shape, name="feat_2_resize")
-    # top_activation
-    feat_3 = deConv2DBatchNorm(base_model.get_layer("top_activation").output, filters=64, name="up0_feat_3")
-    feat_3 = deConv2DBatchNorm(feat_3, filters=32, name="up1_feat_3")
-    feat_3 = deConv2DBatchNorm(feat_3, filters=16, name="up2_feat_3")
-    feat_3 = deConv2DBatchNorm(feat_3, filters=8, name="up3_feat_3")
-    feat_3 = tf.raw_ops.ResizeNearestNeighbor(images=feat_3, size=output_shape, name="feat_3_resize")
+    # features[0]
+    features[0] = tf.raw_ops.ResizeNearestNeighbor(images=features[0], size=output_shape)
+    # features[1]
+    features[1] = deconv_2d(features[1], filters=16)
+    features[1] = tf.raw_ops.ResizeNearestNeighbor(images=features[1], size=output_shape)
+    # features[2]
+    features[2] = deconv_2d(features[2], filters=32)
+    features[2] = deconv_2d(features[2], filters=16)
+    features[2] = tf.raw_ops.ResizeNearestNeighbor(images=features[2], size=output_shape)
+    # features[3]
+    features[3] = deconv_2d(features[3], filters=64)
+    features[3] = deconv_2d(features[3], filters=32)
+    features[3] = deconv_2d(features[3], filters=16)
+    features[3] = tf.raw_ops.ResizeNearestNeighbor(images=features[3], size=output_shape)
+    # features[4]
+    features[4] = deconv_2d(features[4], filters=128)
+    features[4] = deconv_2d(features[4], filters=64)
+    features[4] = deconv_2d(features[4], filters=32)
+    features[4] = deconv_2d(features[4], filters=16)
+    features[4] = tf.raw_ops.ResizeNearestNeighbor(images=features[4], size=output_shape)
 
-    features = tf.keras.layers.concatenate([feat_0, feat_1, feat_2, feat_3], axis=3)
+    features_layer = concatenate([features[0], features[1], features[2], features[3], features[4]], axis=3)
 
     # Create heatmap / class output
-    output_heatmap = tf.keras.layers.Conv2D(32, (3, 3), padding="same", use_bias=False,
-        kernel_initializer=tf.keras.initializers.RandomNormal(0.01), kernel_regularizer=tf.keras.regularizers.l2(1.25e-5),
-        name="heatmap_conv2D")(features)
-    output_heatmap = tf.keras.layers.BatchNormalization(name="heatmap_norm")(output_heatmap)
-    output_heatmap = tf.keras.layers.Activation("relu", name="heatmap_activ")(output_heatmap)
-    output_heatmap = tf.keras.layers.Conv2D(nb_classes, (1, 1), padding="valid", activation=tf.nn.sigmoid,
-        kernel_initializer=tf.keras.initializers.RandomNormal(0.01), kernel_regularizer=tf.keras.regularizers.l2(1.25e-5),
+    output_heatmap = Conv2D(32, (3, 3), padding="same", use_bias=False,
+        kernel_initializer=initializers.RandomNormal(0.01), kernel_regularizer=regularizers.l2(1.25e-5),
+        name="heatmap_conv2D")(features_layer)
+    output_heatmap = BatchNormalization(name="heatmap_norm")(output_heatmap)
+    output_heatmap = ReLU(name="heatmap_activ")(output_heatmap)
+    output_heatmap = Conv2D(nb_classes, (1, 1), padding="valid", activation=tf.nn.sigmoid,
+        kernel_initializer=initializers.RandomNormal(0.01), kernel_regularizer=regularizers.l2(1.25e-5),
         bias_initializer=tf.constant_initializer(-np.log((1.0 - 0.1) / 0.1)), name="heatmap")(output_heatmap)
     
     # Create offset output
-    output_offset = tf.keras.layers.Conv2D(32, (3, 3), padding="same", use_bias=False,
-        kernel_initializer=tf.keras.initializers.RandomNormal(0.001), kernel_regularizer=tf.keras.regularizers.l2(1.25e-5),
-        name="offset_conv2D")(features)
-    output_offset = tf.keras.layers.BatchNormalization(name="offset_norm")(output_offset)
-    output_offset = tf.keras.layers.Activation("relu", name="offset_activ")(output_offset)
-    output_offset = tf.keras.layers.Conv2D(2, (1, 1), padding="valid", activation=None,
-        kernel_initializer=tf.keras.initializers.RandomNormal(0.001), kernel_regularizer=tf.keras.regularizers.l2(1.25e-5),
+    output_offset = Conv2D(32, (3, 3), padding="same", use_bias=False,
+        kernel_initializer=initializers.RandomNormal(0.001), kernel_regularizer=regularizers.l2(1.25e-5),
+        name="offset_conv2D")(features_layer)
+    output_offset = BatchNormalization(name="offset_norm")(output_offset)
+    output_offset = ReLU(name="offset_activ")(output_offset)
+    output_offset = Conv2D(2, (1, 1), padding="valid", activation=None,
+        kernel_initializer=initializers.RandomNormal(0.001), kernel_regularizer=regularizers.l2(1.25e-5),
         name="offset")(output_offset)
 
     # Create size output
-    output_bbox_size = tf.keras.layers.Conv2D(32, (3, 3), padding="same", use_bias=False,
-        kernel_initializer=tf.keras.initializers.RandomNormal(0.001), kernel_regularizer=tf.keras.regularizers.l2(1.25e-5),
-        name="size_conv2D")(features)
-    output_bbox_size = tf.keras.layers.BatchNormalization(name="size_norm")(output_bbox_size)
-    output_bbox_size = tf.keras.layers.Activation("relu", name="size_activ")(output_bbox_size)
-    output_bbox_size = tf.keras.layers.Conv2D(2, (1, 1), padding="valid", activation=None,
-        kernel_initializer=tf.keras.initializers.RandomNormal(0.001), kernel_regularizer=tf.keras.regularizers.l2(1.25e-5),
+    output_bbox_size = Conv2D(32, (3, 3), padding="same", use_bias=False,
+        kernel_initializer=initializers.RandomNormal(0.001), kernel_regularizer=regularizers.l2(1.25e-5),
+        name="size_conv2D")(features_layer)
+    output_bbox_size = BatchNormalization(name="size_norm")(output_bbox_size)
+    output_bbox_size = ReLU(name="size_activ")(output_bbox_size)
+    output_bbox_size = Conv2D(2, (1, 1), padding="valid", activation=None,
+        kernel_initializer=initializers.RandomNormal(0.001), kernel_regularizer=regularizers.l2(1.25e-5),
         name="bounding_box_size")(output_bbox_size)
 
-    # Concatenate output
-    output_layer = tf.keras.layers.concatenate([output_heatmap, output_offset, output_bbox_size], axis=3)
+    # Concatenate output, order is important here!
+    output_layer = concatenate([output_heatmap, output_offset, output_bbox_size], axis=3)
 
     # Create Model
-    model = tf.keras.Model(inputs=base_model.input, outputs=output_layer, name="centernet2d")
+    model = Model(inputs=inputs, outputs=output_layer, name="centernet2d")
 
     return model
--- models/centernet_2d/model_test.py
+++ models/centernet_2d/model_test.py
@@ -3,4 +3,5 @@ from models.centernet_2d import create_model
 
 class TestModel:
     def test_create_model(self):
-        model = create_model(185, 612, int(185 // 2), int(612 // 2), 5)
+        model = create_model(92, 308, int(92 // 2), int(308 // 2), 5)
+        model.summary()
