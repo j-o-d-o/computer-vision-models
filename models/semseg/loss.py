7721f0220413a736259985b868d2e7400a04051b
j-o-d-o <jdobler@protonmail.com>
2021-01-01 15:46:54 +0100

    fix loss

--- models/semseg/loss.py
+++ models/semseg/loss.py
@@ -5,5 +5,5 @@ from tensorflow.keras.losses import Loss, categorical_crossentropy
 class SemsegLoss(Loss):
     def call(self, y_true, y_pred):
         total_loss = categorical_crossentropy(y_true, y_pred, from_logits=True)
-        total_loss = total_loss / 127
+        total_loss = total_loss
         return total_loss
