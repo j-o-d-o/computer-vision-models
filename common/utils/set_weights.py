6c2f9225a6effa21d0a3e2cc326e6f6e155bbcb6
j-o-d-o <jdobler@protonmail.com>
2021-02-28 15:01:44 +0100

    more possibilites to partially set weights

--- common/utils/set_weights.py
+++ common/utils/set_weights.py
@@ -1,25 +1,54 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.python.eager import backprop
 
 
-def set_weights(base_model_path, new_model, custom_objects = {}):
+def layer_weights_have_same_size(layer1, layer2):
+    weights1 = layer1.get_weights()
+    weights2 = layer2.get_weights()
+    if len(weights1) != len(weights2):
+        return False
+    for i in range(len(weights1)):
+        if weights1[i].shape != weights2[i].shape:
+            return False
+    return True
+
+def set_weights(base_model_path, new_model, force_resize=False, custom_objects = {}):
     # Store names of base model layers of model in dict
     base_model = tf.keras.models.load_model(base_model_path, custom_objects=custom_objects, compile=False)
-    base_layer_dict = dict([(layer.name, layer) for layer in base_model.layers]) 
+    base_layer_dict = dict([(layer.name, layer) for layer in base_model.layers])
+
     # Loop through actual model and see if names are matching, set weights in case they are
-    for layer in new_model.layers:
+    for i, layer in enumerate(new_model.layers):
         if layer.name in base_layer_dict:
-            print(f"Setting weights for {layer.name}")
+            print(f"Setting weights for {layer.name} by name")
             try:
                 weights = base_layer_dict[layer.name].get_weights()
                 goal_size = layer.get_weights()
-                for i in range(len(goal_size)):
-                    if goal_size[i].shape != weights[i].shape:
-                        print(f"Need to resize from {weights[i].shape} to {goal_size[i].shape}")
-                        weights[i] = np.resize(weights[i], goal_size[i].shape)
+                for j in range(len(goal_size)):
+                    if goal_size[j].shape != weights[j].shape:
+                        print(f"Need to resize from {weights[j].shape} to {goal_size[j].shape}")
+                        weights[j] = np.resize(weights[j], goal_size[j].shape)
                 layer.set_weights(weights)
             except ValueError as e:
                 print(f"ValueError: {e}")
+        elif len(base_model.layers) > i and layer_weights_have_same_size(base_model.layers[i], layer):
+            print(f"Setting weights for {layer.name} by matching size")
+            layer.set_weights(base_model.layers[i].get_weights())
+        elif len(base_model.layers) > i and force_resize:
+            print(f"Setting weights for {layer.name} by forcing resize")
+            try:
+                weights = base_model.layers[i].get_weights()
+                goal_size = layer.get_weights()
+                for j in range(len(goal_size)):
+                    if goal_size[j].shape != weights[j].shape:
+                        print(f"Need to resize from {weights[j].shape} to {goal_size[j].shape}")
+                        weights[j] = np.resize(weights[j], goal_size[j].shape)
+                layer.set_weights(weights)
+            except (ValueError, IndexError) as e:
+                print(f"ValueError: {e}")
         else:
+            weights1 = layer.get_weights()
+            weights2 = base_model.layers[i].get_weights()
             print(f"Not found: {layer.name}")
     return new_model
